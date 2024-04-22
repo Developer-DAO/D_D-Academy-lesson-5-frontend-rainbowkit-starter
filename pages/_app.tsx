1e7ea7692a9e50abd3cc8cad524d18b54bd2dd13
Brian Gershon <briangershon@users.noreply.github.com>
2024-04-22 09:03:02 -0700

    Remove commented out code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,44 +1,13 @@
-import "../styles/globals.css";
-import "@rainbow-me/rainbowkit/styles.css";
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import "@rainbow-me/rainbowkit/styles.css";
 import type { AppProps } from "next/app";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
-import { arbitrum, mainnet, optimism, polygon, base } from "wagmi/chains";
+import { arbitrum, base, mainnet, optimism, polygon } from "wagmi/chains";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
-
-// import { defineChain } from "viem";
-
-// export const polygonAmoy = /*#__PURE__*/ defineChain({
-//   id: 80002,
-//   name: "Polygon Amoy",
-//   nativeCurrency: { name: "MATIC", symbol: "MATIC", decimals: 18 },
-//   rpcUrls: {
-//     default: {
-//       http: ["https://rpc-amoy.polygon.technology"],
-//     },
-//     public: {
-//       http: ["https://polygon-amoy-bor-rpc.publicnode.com"],
-//     },
-//   },
-//   blockExplorers: {
-//     default: {
-//       name: "OK LINK",
-//       url: "https://www.oklink.com/amoy",
-//     },
-//   },
-//   contracts: {
-//     multicall3: {
-//       address: "0xca11bde05977b3631167028862be2a173976ca11",
-//       blockCreated: 3127388,
-//     },
-//   },
-//   testnet: true,
-//   network: "polygonamoy",
-// });
+import "../styles/globals.css";
 
 import { Chain } from "wagmi";
-import { http } from "viem";
 
 export const polygonAmoy = {
   id: 80002,
